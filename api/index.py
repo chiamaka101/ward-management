d88463e062bc53ae6c88b20c72df07c156a85756
chiamaka101 <jacobchiamaka@gmail.com>
2026-04-28 20:34:35 +0100

    add debug logging

--- api/index.py
+++ api/index.py
@@ -1,6 +1,13 @@
 import os
-from django.core.wsgi import get_wsgi_application
+import sys
+import traceback
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
-app = get_wsgi_application()
+try:
+    from django.core.wsgi import get_wsgi_application
+    app = get_wsgi_application()
+except Exception as e:
+    print("🔥 DJANGO STARTUP ERROR:")
+    traceback.print_exc()
+    raise e
